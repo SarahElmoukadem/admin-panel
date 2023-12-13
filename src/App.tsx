71060af870ae7bab55754cff8316a15550eb4779
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-12-13 08:11:13 +0200

    react router for pages and the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.scss';
+import Menu from './components/menu/Menu';
+import Navbar from './components/navbar/Navbar';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Products from './pages/products/Products';
+
+
+
+
+const Layout = () => {
+  return (
+    <div  className='main'>
+      <Navbar />
+
+      <div className='container'>
+        <div className='side-menu-container'>
+          <Menu />
+        </div>
+
+        <div className='content-container'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      }
+    ]
+  },
+  // {
+  //   path:'/login',
+  //   element:<Login />
+  // }
+
+])
+
 
 function App() {
   return (
     <div className="App">
-  
+      <RouterProvider router={router} />
     </div>
   );
 }
