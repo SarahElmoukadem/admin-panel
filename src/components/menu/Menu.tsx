b09df59d62a6ce3169d09328580cd43f4449c01e
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-12-15 19:34:54 +0200

    add side menu items

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
+import { menu } from "../../data";
+import './menu.scss';
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className="side-menu">
+      {menu.map((item) => (
+        <div className="menu-item" key={item.id}>
+          <span>{item.title}</span>
+
+          {
+            item.listItems.map((listItem) => (
+              <div className="" key={listItem.id}>
+                <Link to={listItem.url} className="listItem" key={listItem.id}>
+                  {/* <span>
+                  {listItem.icon}
+                  </span> */}
+                  <span className="listItemTitle">{listItem.title}</span>
+                </Link>
+              </div>
+            ))
+          }
+        </div>
+      ))}
+    </div>
   )
 }
 
