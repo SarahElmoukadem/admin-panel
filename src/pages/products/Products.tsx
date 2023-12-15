e42fc5b98ec51508ade542c8e220e89ba04256c0
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-12-15 19:38:10 +0200

    products list page

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,8 +1,58 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Product } from '../../interfaces/interface';
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const [data, setData] = useState<Product[] | null>(null);
 
-export default Products
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios('https://dummyjson.com/products');
+        setData(result.data.products);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  // Check if data is an array before mapping
+  if (!Array.isArray(data)) {
+    console.error('Data is not an array:', data);
+    return <div>Error loading data</div>;
+  }
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h4>
+
+          {item.title}
+          </h4>
+
+          <p>
+            {item.description}
+          </p>
+
+          <p>
+            {item.price}
+          </p>
+          <p>
+            {item.discountPercentage}
+          </p>
+          <hr/>
+          </div>
+
+      ))}
+    </div>
+  );
+};
+
+export default Products;
