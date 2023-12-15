3be50e6a95aacbb011916c6629d5ea4d300b6857
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-12-15 19:35:32 +0200

    home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import './home.scss'
+import LineChartBlock from '../../components/lineChartBlock/LineChartBlock'
+import BarChartBlock from '../../components/barChartBlock/BarChartBlock'
+import CustomStraightAnglePieChart from '../../components/customStraightAnglePieChart/CustomStraightAnglePieChart'
+import PieChartBlock from '../../components/pieChartBlock/PieChartBlock'
+import AreaChartBlock from '../../components/areaChartBlock/AreaChartBlock'
 
 const Home = () => {
   return (
-    <div>Home</div>
+
+    <div className='home'>
+
+      <div className="block block1">
+      <BarChartBlock />
+
+      </div>
+
+      <div className="block block2">
+        <LineChartBlock />
+
+      </div>
+
+      <div className="block block3">
+        <PieChartBlock />
+      </div>
+
+      <div className="block block4">
+        <CustomStraightAnglePieChart />
+      </div>
+
+      <div className="block block5">
+        <AreaChartBlock />
+      </div>
+
+      <div className="block block6">
+        {/* <CustomStraightAnglePieChart /> */}
+      </div>
+
+      <div className="block block6">
+        {/* <CustomStraightAnglePieChart /> */}
+      </div>
+
+      <div className="block block2">
+        {/* <CustomStraightAnglePieChart /> */}
+      </div>
+
+    </div>
   )
 }
 
