547eae1ddd6af8dd198193847444259e85443e8d
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-12-18 13:14:55 +0200

    resort charts and add new total counts blocks

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,45 +4,64 @@ import BarChartBlock from '../../components/barChartBlock/BarChartBlock'
 import CustomStraightAnglePieChart from '../../components/customStraightAnglePieChart/CustomStraightAnglePieChart'
 import PieChartBlock from '../../components/pieChartBlock/PieChartBlock'
 import AreaChartBlock from '../../components/areaChartBlock/AreaChartBlock'
+import TotalUserBlock from '../../components/totalUserBlock/TotalUserBlock'
+import TotalPostsBlock from '../../components/totalPostsBlock/TotalPostsBlock'
+import TotalProductsBlock from '../../components/totalProductsBlock/TotalProductsBlock'
+import TotalCartsBlock from '../../components/totalCartsBlock/TotalCartsBlock'
 
 const Home = () => {
   return (
 
     <div className='home'>
 
-      <div className="block block1">
-      <BarChartBlock />
 
+
+      <div className="block block6">
+        <TotalUserBlock />
+        {/* <CustomStraightAnglePieChart /> */}
       </div>
 
-      <div className="block block2">
+      <div className="block block6">
+        <TotalPostsBlock />
+        {/* <CustomStraightAnglePieChart /> */}
+      </div>
+
+      <div className="block block6">
+        {/* <CustomStraightAnglePieChart /> */}
+        <TotalProductsBlock />
+      </div>
+
+      <div className="block block6">
+        {/* <CustomStraightAnglePieChart /> */}
+        <TotalCartsBlock />
+      </div>
+
+
+      <div className="block block1">
         <LineChartBlock />
 
       </div>
 
+      <div className="block block2">
+      <BarChartBlock />
+
+      </div>
+
       <div className="block block3">
-        <PieChartBlock />
+        <AreaChartBlock />
+
       </div>
 
       <div className="block block4">
-        <CustomStraightAnglePieChart />
+        <PieChartBlock />
+
       </div>
 
       <div className="block block5">
-        <AreaChartBlock />
+        <CustomStraightAnglePieChart />
+
       </div>
 
-      <div className="block block6">
-        {/* <CustomStraightAnglePieChart /> */}
-      </div>
-
-      <div className="block block6">
-        {/* <CustomStraightAnglePieChart /> */}
-      </div>
-
-      <div className="block block2">
-        {/* <CustomStraightAnglePieChart /> */}
-      </div>
 
     </div>
   )
