8e4d22bf042076d2a057dc3af62a2a5b0592c7a6
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-12-21 18:26:52 +0200

    todos single page and list page

--- src/pages/todos/Todos.tsx
+++ src/pages/todos/Todos.tsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Todo } from '../../interfaces/interface';
+import TodosDataTable from '../../components/dataTables/todosDataTable/TodosDataTable';
+import Add from '../../components/add/Add';
 
 const Todos = () => {
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'todo',
+      headerName: 'Todo',
+      width: 150,
+      editable: true,
+      type: 'string'
+    },
+    {
+      field: 'completed',
+      headerName: 'completed',
+      type: 'boolean',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'userId',
+      headerName: 'user Id',
+      type: 'number',
+      width: 150,
+      editable: true,
+    }
+  ];
+  
+  const [data, setData] = useState<Todo[] | null>(null);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios('https://dummyjson.com/todos');
+        setData(result.data.todos);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  if (!Array.isArray(data)) {
+    console.error('Data is not an array:', data);
+    return <div>Error loading data</div>;
+  }
+
+  
   return (
-    <div>Todos</div>
+    <div className='products'>
+      <div className='main-info'>
+        <h3>
+          Todos
+        </h3>
+        {/* <button onClick={() => setOpen(true)}>
+          Add new
+        </button> */}
+      </div>
+
+      <TodosDataTable columns={columns} rows={data} slug='todos' />
+
+      {open && <Add setOpen={setOpen} columns={columns} slug='todos'/>}
+
+    </div>
   )
 }
 
---
+++ src/pages/todos/singleTodos/SingleTodos.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './singleProduct.scss';
+import axios from 'axios';
+import { Todo } from '../../../interfaces/interface';
+import { useParams } from 'react-router-dom';
+import { FaCheck } from "react-icons/fa6";
+import { CiTimer } from "react-icons/ci";
+
+const SingleProduct = () => {
+  const [data, setData] = useState<Todo | null>(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(`https://dummyjson.com/todos/${id}`);
+      setData(result.data);
+
+    };
+
+    fetchData();
+
+
+  }, [data]);
+
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className='single-page'>
+      id: <p> {data.id} </p>
+      completed:  <p> {data.completed ? <FaCheck /> : <CiTimer/>}  </p>
+      todo:  <p> {data.todo} </p>
+      user Id:  <p> {data.userId} </p>
+
+    </div>
+  );
+};
+
+export default SingleProduct;
