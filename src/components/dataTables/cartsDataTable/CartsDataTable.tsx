2312e9651ba7e92c9abbe6036d429d904396efe0
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-12-21 18:29:06 +0200

    carts single and list pages

---
+++ src/components/dataTables/cartsDataTable/CartsDataTable.tsx
@@ -0,0 +1,78 @@
+import {
+    DataGrid,
+    GridColDef,
+    GridToolbar,
+} from '@mui/x-data-grid';
+
+import './cartsDataTable.scss';
+import { Link } from 'react-router-dom';
+import { FaRegEye } from "react-icons/fa";
+import { GoTrash } from "react-icons/go";
+import axios from 'axios';
+import { useState } from 'react';
+import { CartsInfo } from '../../../interfaces/interface';
+
+type Props = {
+    columns: GridColDef[],
+    rows: CartsInfo[],
+    slug: string,
+}
+
+const CartsDataTable = (props: Props) => {
+    const [rows, setRows] = useState(props.rows);
+    const handleDelete = (id: number) => {
+        axios.delete(`https://dummyjson.com/carts/${id}`)
+        setRows(rows.filter(item => item.id !== id))
+    }
+    const actionColumn: GridColDef = {
+        field: "action",
+        headerName: "Action",
+        width: 110,
+        renderCell: (params) => {
+            return (
+                <div className='action'>
+                    <Link to={`/carts/${params.row.id}`}>
+                        <FaRegEye />
+                    </Link>
+                    <div className="delete" onClick={() => handleDelete(params.row.id)}>
+                        <GoTrash />
+                    </div>
+                </div>
+            )
+        }
+    }
+
+
+    return (
+        <div className='dataTable'>
+
+            <DataGrid
+                rows={rows}
+                columns={[...props.columns, actionColumn]}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: 10,
+                        },
+                    },
+                }}
+                slots={{ toolbar: GridToolbar }}
+                slotProps={{
+                    toolbar: {
+                        showQuickFilter: true,
+                        quickFilterProps: { debounceMs: 500 },
+                    }
+                }}
+                pageSizeOptions={[5]}
+                checkboxSelection
+                disableRowSelectionOnClick
+                // disableColumnFilter
+                disableColumnSelector
+                disableDensitySelector
+            />
+
+        </div>
+    )
+}
+
+export default CartsDataTable
--- src/pages/carts/Carts.tsx
+++ src/pages/carts/Carts.tsx
@@ -1,8 +1,91 @@
-import React from 'react'
+import { GridColDef } from '@mui/x-data-grid';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Add from '../../components/add/Add';
+import { CartsInfo } from '../../interfaces/interface';
+import CartsDataTable from '../../components/dataTables/cartsDataTable/CartsDataTable';
 
 const Carts = () => {
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'total',
+      headerName: 'total',
+      width: 150,
+      editable: true,
+      type: 'string'
+    },
+    {
+      field: 'discountedTotal',
+      headerName: 'Discounted Total',
+      type: 'number',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'userId',
+      headerName: 'userId',
+      type: 'number',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'totalProducts',
+      headerName: 'total Products',
+      type: 'number',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'totalQuantity',
+      headerName: 'total Quantity',
+      type: 'number',
+      width: 150,
+      editable: true,
+    }
+  ];
+
+  const [data, setData] = useState<CartsInfo[] | null>(null);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios('https://dummyjson.com/carts');
+        setData(result.data.carts);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  if (!Array.isArray(data)) {
+    console.error('Data is not an array:', data);
+    return <div>Error loading data</div>;
+  }
+  
   return (
-    <div>Carts</div>
+    <div>
+    <div className='main-info'>
+      <h3>
+        Carts
+      </h3>
+      {/* <button onClick={() => setOpen(true)}>
+    Add new
+  </button> */}
+    </div>
+
+    <CartsDataTable columns={columns} rows={data} slug='carts' />
+
+    {open && <Add setOpen={setOpen} columns={columns} slug='carts' />}
+
+  </div>
   )
 }
 
